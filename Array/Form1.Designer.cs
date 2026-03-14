0439bc467f53d99585bfd216c76c4835b7481b64
asus <asus@LAPTOP-T5SPT3N3>
2026-03-14 23:27:08 +0800

    Add project files.

---
+++ Array/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Array
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(53, 53);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(156, 124);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += Form1_Load;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 15F);
+            button1.Location = new Point(274, 279);
+            button1.Name = "button1";
+            button1.Size = new Size(202, 78);
+            button1.TabIndex = 1;
+            button1.Text = "I dare you to Click me!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(238, 219);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(277, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(346, 103);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 3;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+    }
+}
---
+++ Array/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Array
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			this.Load += Form1_Load;	
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+			string[] names = { "Rhobbie", "Jhirro", "Raine", "Jolo", "Prince", "Mark" };
+
+			listBox1.Items.Clear();
+			listBox1.Items.AddRange(names);
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string message = (textBox1.Text ?? string.Empty).ToLower();
+
+			if (message.Contains("hello"))
+			{
+				label1.Text = "There it is - hello -";
+			}
+			else
+			{
+				label1.Text = "No - hello - ";
+
+			}
+			char[,] letters =
+			{
+				{ 'a', 'b', 'c' },
+				{ 'd', 'e', 'f' }
+			};
+			string twoD = letters[0, 0] + " " + letters[0, 1] + " " + letters[0, 2] + "\n" +
+					  letters[1, 0] + " " + letters[1, 1] + " " + letters[1, 2];
+			MessageBox.Show(twoD, "2D Letters");
+		}
+	}
+}
